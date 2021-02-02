2983f0b6addba7ffd076ea0d702ac4ce8d86b347
Jon Mulderije <jmapps2020@gmail.com>
2021-02-02 13:36:53 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jon Mulderije on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
